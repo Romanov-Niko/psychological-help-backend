7af1b382f0e97544c27a36dcc03cc4baa78dacf9
Mykola Romanov <m.romanov@gethatch.com>
2022-11-12 17:30:22 +0200

    Empty spring boot app

---
+++ src/main/java/com/kpi/PsychologicalHelpBackendApplication.java
@@ -0,0 +1,12 @@
+package com.kpi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsychologicalHelpBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PsychologicalHelpBackendApplication.class, args);
+  }
+}
